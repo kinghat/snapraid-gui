733bf3cf46d9d617bc2dae574a1dcf001ea61dab
kinghat <kinghat@protonmail.com>
2021-05-23 19:09:47 -0500

    commit all

--- apps/api/.devcontainer/config/_entry.sh
+++ apps/api/.devcontainer/config/_entry.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
+# freedomLoops() {
+#   FILTER='^loop'
+#   lsblk --raw -a --output "NAME,MAJ:MIN" --noheadings | grep -E "$FILTER" | while read LINE; do
+#     DEV=/dev/$(echo "$LINE" | cut -d' ' -f1)
+#     MAJMIN=$(echo "$LINE" | cut -d' ' -f2)
+#     MAJ=$(echo "$MAJMIN" | cut -d: -f1)
+#     MIN=$(echo "$MAJMIN" | cut -d: -f2)
+#     [ -b "$DEV" ] || mknod "$DEV" b "$MAJ" "$MIN"
+#   done
+# }
+
+# freedomLoops
 sudo mount /mnt/loops/disk00.img /mnt/snapraid/disk00
+# freedomLoops
 sudo mount /mnt/loops/disk01.img /mnt/snapraid/disk01
+# freedomLoops
 sudo mount /mnt/loops/disk02.img /mnt/snapraid/disk02
+# freedomLoops
 sudo mount /mnt/loops/disk03.img /mnt/snapraid/disk03
+# freedomLoops
 sudo mount /mnt/loops/parity00.img /mnt/snapraid/parity00
+# freedomLoops
 sudo mount /mnt/loops/parity01.img /mnt/snapraid/parity01
 sudo chown -R deno:deno /mnt/snapraid/disk*
 
