9d80da4c7bc615cb80850546f6f07cd9b619d4cc
kinghat <kinghat@protonmail.com>
2021-05-24 11:42:32 -0500

    commit all

--- apps/api/.devcontainer/config/_entry.sh
+++ apps/api/.devcontainer/config/_entry.sh
@@ -11,17 +11,11 @@
 #   done
 # }
 
-# freedomLoops
 sudo mount /mnt/loops/disk00.img /mnt/snapraid/disk00
-# freedomLoops
 sudo mount /mnt/loops/disk01.img /mnt/snapraid/disk01
-# freedomLoops
 sudo mount /mnt/loops/disk02.img /mnt/snapraid/disk02
-# freedomLoops
 sudo mount /mnt/loops/disk03.img /mnt/snapraid/disk03
-# freedomLoops
 sudo mount /mnt/loops/parity00.img /mnt/snapraid/parity00
-# freedomLoops
 sudo mount /mnt/loops/parity01.img /mnt/snapraid/parity01
 sudo chown -R deno:deno /mnt/snapraid/disk*
 
