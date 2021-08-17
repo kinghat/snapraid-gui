716250a194ce54d76d859999a40df52de522e54c
kinghat <kinghat@protonmail.com>
2021-08-17 16:52:40 -0500

    update deps

--- api/deps.ts
+++ api/deps.ts
@@ -5,7 +5,7 @@ import {
   Context,
   Router,
   RouterContext,
-} from "https://deno.land/x/oak@v8.0.0/mod.ts";
+} from "https://deno.land/x/oak@v9.0.0/mod.ts";
 // export { Application, Router, Context } from "https://deno.land/x/oak/mod.ts";
 export { Application, Context, Router };
 // export type { RouterContext } from "https://deno.land/x/oak/mod.ts";
@@ -16,9 +16,9 @@ export {
   exists,
   move,
   walk,
-} from "https://deno.land/std@0.104.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.104.0/path/mod.ts";
-export { parse } from "https://deno.land/std@0.104.0/flags/mod.ts";
+} from "https://deno.land/std@0.105.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.105.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.105.0/flags/mod.ts";
 // export {
 //   Line,
 //   Option,
