35562b05264acb0e6ddfc15a0d98ea65c4fee091
kinghat <kinghat@protonmail.com>
2021-09-24 12:20:46 -0500

    commit all

--- api/src/db/db.ts
+++ api/src/db/db.ts
@@ -1,1 +1,7 @@
-// TODO: setup db
+import { Database, SQLite3Connector } from "../../deps.ts";
+
+const connector = new SQLite3Connector({
+  filepath: "./database.sqlite",
+});
+
+const db = new Database(connector);
