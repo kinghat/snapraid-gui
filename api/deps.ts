607dfb0b66d5d4eaef7f5f2c032034c3c0b1fa64
kinghat <kinghat@protonmail.com>
2021-11-17 13:14:50 -0600

    update deps

--- api/deps.ts
+++ api/deps.ts
@@ -4,25 +4,26 @@ export { config } from "https://deno.land/x/dot_env@0.2.0/mod.ts";
 import {
   Application,
   Context,
+  Middleware,
   Router,
   RouterContext,
   RouterMiddleware,
   Status,
-} from "https://deno.land/x/oak@v9.0.1/mod.ts";
+} from "https://deno.land/x/oak@v10.0.0/mod.ts";
 // export { Application, Router, Context } from "https://deno.land/x/oak/mod.ts";
 export { Application, Context, Router, Status };
 // export type { RouterContext } from "https://deno.land/x/oak/mod.ts";
-export type { RouterContext, RouterMiddleware };
+export type { Middleware, RouterContext, RouterMiddleware };
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 export {
-  copy,
   emptyDir,
   exists,
   move,
   walk,
-} from "https://deno.land/std@0.114.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.114.0/path/mod.ts";
-export { parse } from "https://deno.land/std@0.114.0/flags/mod.ts";
+} from "https://deno.land/std@0.115.0/fs/mod.ts";
+export { copy } from "https://deno.land/std@0.115.0/fs/copy.ts";
+export * as path from "https://deno.land/std@0.115.0/path/mod.ts";
+export { parse } from "https://deno.land/std@0.115.0/flags/mod.ts";
 // export * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts";
 // export {
 //   create,
@@ -48,4 +49,4 @@ export {
 export {
   Session,
   SqliteStore,
-} from "https://deno.land/x/oak_sessions@v3.2.0/mod.ts";
+} from "https://deno.land/x/oak_sessions@v3.2.1/mod.ts";
